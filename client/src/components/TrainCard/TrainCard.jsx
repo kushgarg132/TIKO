ae758323d57867d2d1a81c656dbb82dfeb55bcd7
sreemanrepaka <91213639+sreemanrepaka@users.noreply.github.com>
2023-05-07 16:43:25 +0530

    added props

--- client/src/components/TrainCard/TrainCard.jsx
+++ client/src/components/TrainCard/TrainCard.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./TrainCards.css";
 
-const TrainCard = () => {
+const TrainCard = (props) => {
   return (
     <div className="train-card">
       <div className="train-card-header">
-        <div className="train-name">Irctc Tejas Exp</div>
-        <div className="train-number">Train No: #82502</div>
+        <div className="train-name">{props.train.name}</div>
+        <div className="train-number">#{props.train.number}</div>
         <div className="train-days">
           <div className="train-day">
             <span className="day-label">S</span>
@@ -34,21 +34,21 @@ const TrainCard = () => {
       <div className="train-card-body">
         <div className="train-info">
           <div className="train-info-left">
-            <div className="train-departure-time">3:40 PM, Sun</div>
+            <div className="train-departure-time">{props.train.departure}</div>
             <div className="train-departure-station">
-              New Delhi Railway Station (NDLS)
+              {props.train.dep_station}
             </div>
           </div>
           <div className="train-info-left-left">➔</div>
           <div className="train-info-middle">
-            <div className="train-duration">4h 55m</div>
+            <div className="train-duration">{props.train.duration}</div>
             <div className="train-view-route">View route</div>
           </div>
           <div className="train-info-right-right">➔</div>
           <div className="train-info-right">
-            <div className="train-arrival-time">8:35 PM, Sun</div>
+            <div className="train-arrival-time">{props.train.arrival}</div>
             <div className="train-arrival-station">
-              Kanpur Central Railway Station (CNB)
+              {props.train.arr_station}
             </div>
           </div>
         </div>
@@ -56,27 +56,25 @@ const TrainCard = () => {
           <div className="train-subscription">
             <div className="subscription-left">
               <div className="subscription-class">SL</div>
-              <div className="subscription-price">&#x20B9;1985</div>
+              <div className="subscription-price">&#x20B9;{props.train.slprice}</div>
             </div>
             <div className="subscription-right">
               <div className="subscription-availability available">
-                Available 237
+                Available: {props.train.sleeper}
               </div>
-              <div className="subscription-cancellation">Free Cancellation</div>
-              <div className="subscription-updated">Updated 1 hr ago</div>
+              
             </div>
           </div>
           <div className="train-subscription">
             <div className="subscription-left">
               <div className="subscription-class">AC</div>
-              <div className="subscription-price">&#x20B9;2394</div>
+              <div className="subscription-price">&#x20B9;{props.train.acprice}</div>
             </div>
             <div className="subscription-right">
               <div className="subscription-availability available">
-                Available 3
+                Available: {props.train.ac}
               </div>
-              <div className="subscription-cancellation">Free Cancellation</div>
-              <div className="subscription-updated">Updated 9 hrs ago</div>
+              
             </div>
           </div>
         </div>
