9fcdd5571d86623a3488ea2e05db464b169a0287
Akshansh Sourabh <78357333+SourHask@users.noreply.github.com>
2023-05-07 12:53:57 +0530

    All Updated

--- client/src/Pages/Availability/Availability.jsx
+++ client/src/Pages/Availability/Availability.jsx
@@ -1,6 +1,8 @@
 import SearchBo from "../../components/SearchBar/SearchB";
 // import "../components/SearchBar/SearchB.css";
 import Navbar from "../../components/Navbar/Navbar";
+import TrainCard from "../../components/TrainCard/TrainCard";
+import FilteringComponent from "./Filters";
 
 const Availability = () => {
   
@@ -8,7 +10,14 @@ const Availability = () => {
   return (
     <>
         <Navbar/>
-        <SearchBo/>
+        <div style={{ display: 'flex' }}>
+          <SearchBo/>
+          <FilteringComponent/>
+        </div>
+        <TrainCard/>
+        <TrainCard/>
+
+
     </>
   );
 };
