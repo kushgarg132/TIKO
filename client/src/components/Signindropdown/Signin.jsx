2b9b59c91adba337e1981d28e5b927803318b6b5
Anindita-172 <animuk1702@gmail.com>
2023-05-08 13:30:28 +0530

    Drop down for Profile

---
+++ client/src/components/Signindropdown/Signin.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Signin.css';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="box">
+        <h6>Welcome Kush!</h6>
+              <div className="dropdown">
+                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                 <Link className="dropdown-item" to="/profile">Your Profile</Link>
+                      
+            <Link className="dropdown-item" to="/logout">Log Out</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
