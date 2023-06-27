4b7b399459745e4ce6699bbc8b9c841e9dd1dd30
Tanmay Yadav <tanmayyadav2323@gmail.com>
2023-06-27 21:13:18 +0530

    Feat Backend : Added Journey Day

--- server/controllers/trains.js
+++ server/controllers/trains.js
@@ -10,25 +10,51 @@ export const getAllTrains = async (req, res) => {
 };
 
 export const getTrain = async (req, res) => {
-  let { source, destination, journeyDay , today } = req.body;
+  let { source, destination, journeyDay } = req.body;
   var weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   try {
     const trainList = [];
+    // source = 'Amritsar';
+    // destination = 'Jammu';
+    // journeyDay = 'Saturday';
 
     const trains = await Trains.find({
-      journeyDay: { $elemMatch: { $eq: journeyDay } }
     });
+
+    let journeyIndex = -1;
+    for (let q = 0; q < weekday.length; q++) {
+      if (journeyDay == weekday[q]) {
+        journeyIndex = q;
+        break;
+      }
+    }
 
     for (let i = 0; i < trains.length; i++) {
       let flag = -1;
+
+      let trainDaysIndex = [];
+      for (let p = 0; p < trains[i].journeyDay.length; p++) {
+        for (let q = 0; q < weekday.length; q++) {
+          if(trains[i].journeyDay[p] == weekday[q]){
+            trainDaysIndex.push(q);
+          }
+        }
+      }
       for (let j = 0; j < trains[i].route.length; j++) {
-        if (trains[i].route[j].stationName == source) {
-          flag = j;
-          break;
+        if (trains[i].route[j].stationName == source
+        ) {
+          for(let d  = 0; d<trainDaysIndex.length; d++){
+            console.log(trainDaysIndex[d],trains[i].route[j].arrivalDay,journeyIndex);
+            if((trainDaysIndex[d] + trains[i].route[j].arrivalDay-1)%7  == journeyIndex){
+              flag = j;
+              break;
+            }
+          }
         }
       }
 
-      for (let j = flag+1; j < trains[i].route.length; j++) {
+      if(flag == -1)continue;
+      for (let j = flag + 1; j < trains[i].route.length; j++) {
         if (trains[i].route[j].stationName == destination) {
           flag = j;
           trainList.push(trains[i]);
